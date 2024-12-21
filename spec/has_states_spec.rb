227748d0ea1db809a6f4c9f3891b6e4e3df5640d
Sebastian Scholl <sebscholl@gmail.com>
2024-12-21 11:26:25 -0500

    Init

---
+++ spec/has_states_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HasStates do
+  it "has a version number" do
+    expect(HasStates::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
